53b2527428cf31fb26dd7d922917af2e8baec0fb
LeoRaab <leopold.raab@gmail.com>
2022-05-22 11:12:44 +0200

    add auth middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,31 @@
+import {Request, Response, NextFunction} from 'express';
+import * as jwt from 'jsonwebtoken';
+import HttpError from '../models/http-error';
+
+interface IUserToken {
+    userId: string
+    email: string
+}
+
+const auth = (req: Request, res: Response, next: NextFunction) => {
+
+    if (!req.headers.authorization) {
+        return next(new HttpError('Authentication failed', 401));
+    }
+
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if (!token) {
+            return next(new HttpError('Authentication failed!', 401));
+        }
+
+        const decodedToken = jwt.verify(token, process.env.JWT_KEY!) as IUserToken;
+        req.userId = decodedToken.userId;
+        return next();
+
+    } catch (e) {
+        return next(new HttpError('Authentication failed!', 401));
+    }
+}
+
+export default auth;
