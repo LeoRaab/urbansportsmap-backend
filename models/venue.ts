0fc2b5fe085bdbd3671853789ad948c07cb92f6c
LeoRaab <leopold.raab@gmail.com>
2022-06-07 17:59:13 +0200

    added image upload

--- models/venue.ts
+++ models/venue.ts
@@ -1,5 +1,6 @@
 import * as mongoose from 'mongoose';
-import {ICommentDoc} from './comment';
+import { ICommentDoc } from './comment';
+import { IVenueImageDoc } from './venue-image';
 
 const Schema = mongoose.Schema;
 
@@ -13,17 +14,19 @@ export interface IVenue {
 }
 
 export interface IVenueDoc extends IVenue, mongoose.Document {
-    comments: mongoose.Types.Array<ICommentDoc>
+    comments: mongoose.Types.Array<ICommentDoc>,
+    images: mongoose.Types.Array<IVenueImageDoc>,
 }
 
 const venueSchema = new Schema<IVenueDoc>({
-    name: { type: String, required: true},
+    name: { type: String, required: true },
     location: {
-        lat: { type: Number, required: true},
-        lng: { type: Number, required: true}
+        lat: { type: Number, required: true },
+        lng: { type: Number, required: true }
     },
-    sportTypes: [{ type: String, required: true}],
-    comments: [{ type: Schema.Types.ObjectId, required: true, ref: 'Comment'}]
+    sportTypes: [{ type: String, required: true }],
+    comments: [{ type: Schema.Types.ObjectId, required: true, ref: 'Comment' }],
+    images: [{ type: Schema.Types.ObjectId, required: true, ref: 'Image' }],
 }, { timestamps: true })
 
 const Venue = mongoose.model<IVenueDoc>('Venue', venueSchema);
