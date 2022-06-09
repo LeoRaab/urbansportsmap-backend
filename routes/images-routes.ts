70725f668ec69d7ddc85b0fc44a5fc42f0b7d28f
LeoRaab <leopold.raab@gmail.com>
2022-06-09 16:00:33 +0200

    added check-upload-path middleware

--- routes/images-routes.ts
+++ routes/images-routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import fileUpload from "../middleware/file-upload";
 import * as imagesController from '../controllers/images-controller';
+import auth from "../middleware/auth";
+import checkUploadPath from "../middleware/check-upload-path";
 
 const router = Router();
 
-router.post('/:venueId', fileUpload.single('image'), imagesController.uploadImage);
+router.use(auth);
+
+router.post('/:venueId', checkUploadPath, fileUpload.single('image'), imagesController.uploadImage);
 
 export default router;
