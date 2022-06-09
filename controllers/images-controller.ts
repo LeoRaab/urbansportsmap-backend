a485e52c50b0a08305a71be4091d76ab66ef9cc0
LeoRaab <leopold.raab@gmail.com>
2022-06-09 16:09:17 +0200

    added getImagesByVenue function

--- controllers/images-controller.ts
+++ controllers/images-controller.ts
@@ -1,10 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import MESSAGES from "../constants/messages";
 import HttpError from "../models/http-error";
-import VenueImage from "../models/venue-image";
+import VenueImage, { IVenueImageDoc } from "../models/venue-image";
 import ImagesRepository from "../repositories/images-repository";
 
 const imagesRepository = new ImagesRepository();
+
+const getImagesByVenue = async (req: Request, res: Response, next: NextFunction) => {
+    const venueId = req.params.venueId;
+
+    const { result: images, error } = await imagesRepository.readAll({ venue: venueId });
+
+    if (error) {
+        return next(error);
+    }
+
+    if (!images) {
+        return next(new HttpError(MESSAGES.NO_DATA_FOUND, 404));
+    }
+
+    res.json({
+        images: images.map((image: IVenueImageDoc) => image.toObject({ getters: true }))
+    });
+}
 
 const uploadImage = async (req: Request, res: Response, next: NextFunction) => {
     /*
@@ -42,5 +60,6 @@ const uploadImage = async (req: Request, res: Response, next: NextFunction) => {
 }
 
 export {
+    getImagesByVenue,
     uploadImage
 }
