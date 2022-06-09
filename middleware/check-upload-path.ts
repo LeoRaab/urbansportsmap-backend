feabea2927af2654a4d453f65f126090eb8f17ec
LeoRaab <leopold.raab@gmail.com>
2022-06-09 15:59:18 +0200

    added check-upload-path middleware

---
+++ middleware/check-upload-path.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import * as fs from 'fs';
+import MESSAGES from "../constants/messages";
+import HttpError from "../models/http-error";
+
+const checkUploadPath = (req: Request, res: Response, next: NextFunction) => {
+    const venueId = req.params.venueId;
+
+    if (!venueId) {
+        next(new HttpError(MESSAGES.CREATE_FAILED, 404));
+    }
+
+    const uploadPath = 'uploads/images/venues/' + venueId;
+
+    const isUploadPathExisting = fs.existsSync(uploadPath);
+
+    if (!isUploadPathExisting) {
+        try {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        } catch (e) {
+            console.error(e);
+            next(new HttpError(MESSAGES.CREATE_FAILED, 500));
+        }
+    }
+
+    next();
+}
+
+export default checkUploadPath;
