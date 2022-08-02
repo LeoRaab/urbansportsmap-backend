530cdba239b72436a47a4ca129281958b524612a
LeoRaab <leopold.raab@gmail.com>
2022-08-02 18:21:23 +0200

    changed image size

---
+++ util/resize-image.ts
@@ -0,0 +1,17 @@
+import sharp = require('sharp');
+
+const resizeImage = async (image: Express.Multer.File): Promise<Buffer | undefined> => {
+  try {
+    return await sharp(image.buffer)
+      .resize(1600, 1200, {
+        kernel: sharp.kernel.nearest,
+        fit: 'cover'
+      })
+      .jpeg({ quality: 95 })
+      .toBuffer();
+  } catch (e) {
+    //Log error!
+  }
+};
+
+export default resizeImage;
